0800514216b9fac2ad929b155d491e2b003434ab
Sergio Delgadillo <sdelgadillo@farmkd.com>
2022-08-29 22:04:23 -0300

    init poject with dependecies

---
+++ src/main/java/com/desapp/cryptop2p/Cryptop2pApplication.java
@@ -0,0 +1,13 @@
+package com.desapp.cryptop2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cryptop2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cryptop2pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desapp/cryptop2p/Cryptop2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desapp.cryptop2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cryptop2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
